8e52218f382f59768614822e660fc721b755d2d7
Diepiriye Okujagu <diepiriye.d.okujagu@ntnu.no>
2023-06-16 11:07:13 +0200

    docker setup

---
+++ setup/jupyter_notebook_config.py
@@ -0,0 +1,14 @@
+c = get_config()  # noqa
+
+# load base config
+load_subconfig("/etc/jupyter/jupyter_notebook_config_base.py")  # noqa
+
+# supports iframe and samesite cookies
+c.NotebookApp.tornado_settings = {
+    "headers": {"Content-Security-Policy": "frame-ancestors 'self' *"},
+    "cookie_options": {"SameSite": "None", "Secure": True},
+}
+c.NotebookApp.allow_root = True
+c.NotebookApp.allow_origin = "*"
+c.NotebookApp.token = ""
+# c.NotebookApp.default_url = "/lab"
